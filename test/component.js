d694706771c94de7f6a24996d7964fc04065c974
benox3 <ben.bj32001@gmail.com>
2016-10-05 15:39:12 -0400

    Add component tests
    - Ensure no img rendered if props are not passed
    - Ensure setResponsiveImage gets called on componentDidMount

--- test/component.js
+++ test/component.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Pic from '../lib/';
+import { spy } from 'sinon';
 
 describe('Pic', function() {
   it('should render the first image in images', function() {
@@ -53,5 +54,34 @@ describe('Pic', function() {
         style={pic.props().style}
         src={props.images[1].url} />
     )).to.equal(true);
+  });
+
+  it('should not render image if props are not passed', function() {
+
+    const pic = shallow(
+      <Pic />
+    );
+
+    expect(pic.find('img')).to.have.length(0);
+  });
+
+  it('should setResponsiveImage image once mounted', function() {
+    const props = {
+      images: [
+        {
+          width: 290,
+          url: 'http://placehold.it/290?text=♥'
+        },
+        {
+          width: 320,
+          url: 'http://placehold.it/320?text=♥'
+        }
+      ]
+    };
+
+    spy(Pic.prototype, 'setResponsiveImage');
+    mount(<Pic { ...props } />);
+
+    expect(Pic.prototype.setResponsiveImage).to.have.property('callCount', 1);
   });
 });
