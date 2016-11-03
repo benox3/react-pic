d23efb0cd3380ea5a6b3384f4f2fa5ff72466f0b
benox3 <ben.bj32001@gmail.com>
2016-11-03 16:54:57 -0400

    Add button that toggles `<Pic>` to make it easier to develop for unmount

--- playground/index.js
+++ playground/index.js
@@ -4,6 +4,18 @@ import React, { Component } from 'react';
 import Pic from '../lib/index.js';
 
 export default class Playground extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    this.setState({show:!this.state.show});
+  }
+
   render() {
     return (
       <html>
@@ -12,31 +24,37 @@ export default class Playground extends Component {
           <title>react-pic</title>
         </head>
         <body>
-          <div id="root">
-            <Pic
-                alt='winky face'
-                images={[
-                  {
-                    width: 40,
-                    url: 'http://placehold.it/40?text=😉'
-                  },
-                  {
-                    width: 200,
-                    url: 'http://placehold.it/200?text=😉'
-                  },
-                  {
-                    width: 400,
-                    url: 'http://placehold.it/400?text=😉'
-                  },
-                  {
-                    width: 600,
-                    url: 'http://placehold.it/600?text=😉'
-                  },
-                  {
-                    width: 800,
-                    url: 'http://placehold.it/800?text=😉'
-                  }
-                ]} />
+          <div id="root" style={{height:1500}}>
+            <button onClick={this.clickHandler}>
+              Toggle
+            </button>
+            {
+              this.state.show &&
+              <Pic
+                  alt='winky face'
+                  images={[
+                    {
+                      width: 40,
+                      url: 'http://placehold.it/40?text=😉'
+                    },
+                    {
+                      width: 200,
+                      url: 'http://placehold.it/200?text=😉'
+                    },
+                    {
+                      width: 400,
+                      url: 'http://placehold.it/400?text=😉'
+                    },
+                    {
+                      width: 600,
+                      url: 'http://placehold.it/600?text=😉'
+                    },
+                    {
+                      width: 800,
+                      url: 'http://placehold.it/800?text=😉'
+                    }
+                  ]} />
+            }
           </div>
           <script src='//localhost:8080/build/react-pic.js' />
         </body>
