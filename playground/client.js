95a6b9f5cc348074ae8944aaf69526720ef8bea0
Ben Budnevich <ben.bj32001@gmail.com>
2017-04-10 01:32:29 -0400

    Refactor playground

--- playground/client.js
+++ playground/client.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './index';
 
-ReactDOM.render(
-  React.createElement(
-    Component
-  ), document
-);
+ReactDOM.render(React.createElement(Component), document);
